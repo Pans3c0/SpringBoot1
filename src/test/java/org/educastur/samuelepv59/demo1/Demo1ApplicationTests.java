9cc233b81db93f6f132ebc38ce62eaee4aee1087
pans3c0 <samupachebq@gmail.com>
2025-03-08 21:33:35 +0100

    Primer commit

---
+++ src/test/java/org/educastur/samuelepv59/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.educastur.samuelepv59.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
